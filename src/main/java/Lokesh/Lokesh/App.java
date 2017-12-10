3cbdb69a7d55b8ffc528edc21639c71b091dfadd
LokeshKumaK <k.lokeshmail1@gmail.com>
2017-12-09 20:07:23 -0800

    first commit

---
+++ src/main/java/Lokesh/Lokesh/App.java
@@ -0,0 +1,12 @@
+package Lokesh.Lokesh;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
